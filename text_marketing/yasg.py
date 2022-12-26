a934d8e70a89e363dc9cbb67fbc12580df8b2daa
slava kondratieph <kondratieph@gmail.com>
2022-12-26 18:53:03 +0600

    yasg.py updated

--- text_marketing/yasg.py
+++ text_marketing/yasg.py
@@ -15,9 +15,9 @@ schema_view = get_schema_view(
     permission_classes=[permissions.AllowAny, ],
 )
 urlpatterns = [
-    path('swagger(?P<format>\.json|\.yaml)',
+    path('docs(?P<format>\.json|\.yaml)',
          schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/',
+    path('docs/',
          schema_view.with_ui('swagger', cache_timeout=0),
          name='schema-swagger-ui'),
     path('redoc/',
